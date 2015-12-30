3639fd7fe3115ca3b621064c22c216820278b735
Martin Lindhe <martin@ubique.se>
2015-12-30 19:52:10 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# About
+
+Command line to validate YAML syntax of input files.
+This tool simply exposes the super fast gopkg.in/yaml.v2 to the command line.
+
+
+# Installation
+
+    go install github.com/martinlindhe/validyaml
+
+
+
+# License
+
+Under [MIT](LICENSE)
---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"gopkg.in/yaml.v2"
+)
+
+func main() {
+
+	if len(os.Args) < 2 {
+		fmt.Printf("Usage: %s <file.yml>\n", filepath.Base(os.Args[0]))
+		os.Exit(0)
+	}
+
+	inFile := os.Args[1]
+
+	if !fileExists(inFile) {
+		fmt.Printf("Error: file %s not found.\n", inFile)
+		os.Exit(1)
+	}
+
+	data, _ := ioutil.ReadFile(inFile)
+
+	var f interface{}
+	err := yaml.Unmarshal(data, &f)
+	if err != nil {
+		fmt.Printf("YAML error: %s\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("OK: %s\n", inFile)
+}
+
+func fileExists(name string) bool {
+
+	if _, err := os.Stat(name); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
+}
