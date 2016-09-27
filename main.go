f37fc698c2c2f8fa18d29ba115c50af74f7da31f
Martin Lindhe <martin-commit@ubique.se>
2016-09-27 15:15:57 +0200

    also accept data from stdin

--- main.go
+++ main.go
@@ -5,12 +5,13 @@ import (
 	"io/ioutil"
 	"os"
 
+	termutil "github.com/andrew-d/go-termutil"
 	"gopkg.in/alecthomas/kingpin.v2"
 	"gopkg.in/yaml.v2"
 )
 
 var (
-	inFile = kingpin.Arg("file", "YAML file.").Required().ExistingFile()
+	inFile = kingpin.Arg("file", "YAML file.").String()
 	pretty = kingpin.Flag("pretty", "Pretty print result.").Short('p').Bool()
 	quiet  = kingpin.Flag("quiet", "Don't output on success.").Short('q').Bool()
 )
@@ -21,10 +22,14 @@ func main() {
 	kingpin.CommandLine.HelpFlag.Short('h')
 	kingpin.Parse()
 
-	data, _ := ioutil.ReadFile(*inFile)
+	data, err := readPipeOrFile(*inFile)
+	if err != nil {
+		fmt.Println("error:", err)
+		os.Exit(1)
+	}
 
 	var f interface{}
-	err := yaml.Unmarshal(data, &f)
+	err = yaml.Unmarshal(data, &f)
 	if err != nil {
 		fmt.Println("ERROR:", *inFile, err)
 		os.Exit(1)
@@ -36,10 +41,20 @@ func main() {
 			fmt.Println("ERROR:", err)
 		}
 		fmt.Printf(string(b))
-
 	} else {
 		if !*quiet {
 			fmt.Println("OK:", *inFile)
 		}
 	}
+}
+
+// readPipeOrFile reads from stdin if pipe exists, else from provided file
+func readPipeOrFile(fileName string) ([]byte, error) {
+	if !termutil.Isatty(os.Stdin.Fd()) {
+		return ioutil.ReadAll(os.Stdin)
+	}
+	if fileName == "" {
+		return nil, fmt.Errorf("no piped data and no file provided")
+	}
+	return ioutil.ReadFile(fileName)
 }
